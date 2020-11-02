1764908033921901057edb77066600b98da6d7b4
PvUtrix <32957084+PvUtrix@users.noreply.github.com>
2020-11-02 16:41:31 +0300

    Update interface_config.js

--- interface_config.js
+++ interface_config.js
@@ -46,9 +46,10 @@ var interfaceConfig = {
 
     DEFAULT_BACKGROUND: '#474747',
     DEFAULT_LOCAL_DISPLAY_NAME: 'me',
-    DEFAULT_LOGO_URL: 'images/watermark.png',
-    DEFAULT_REMOTE_DISPLAY_NAME: 'Fellow Jitster',
-    DEFAULT_WELCOME_PAGE_LOGO_URL: 'images/watermark.png',
+    DEFAULT_LOGO_URL: 'https://tangovision.s3.eu-central-1.amazonaws.com/assets/tango-vision-logo-triangle.png',
+    DEFAULT_REMOTE_DISPLAY_NAME: 'Fellow',
+    /*DEFAULT_WELCOME_PAGE_LOGO_URL: 'images/watermark.png',*/
+    DEFAULT_WELCOME_PAGE_LOGO_URL: 'https://tangovision.s3.eu-central-1.amazonaws.com/assets/tango-vision-logo-triangle.png',
 
     DISABLE_DOMINANT_SPEAKER_INDICATOR: false,
 
@@ -113,9 +114,10 @@ var interfaceConfig = {
     HIDE_INVITE_MORE_HEADER: false,
 
     INITIAL_TOOLBAR_TIMEOUT: 20000,
-    JITSI_WATERMARK_LINK: 'https://jitsi.org',
+    JITSI_WATERMARK_LINK: 'https://meet.tango.vision',
 
     LANG_DETECTION: true, // Allow i18n to detect the system language
+/*    LIVE_STREAMING_HELP_LINK: 'https://jitsi.org/live', // Documentation reference for the live streaming feature.*/
     LIVE_STREAMING_HELP_LINK: 'https://jitsi.org/live', // Documentation reference for the live streaming feature.
     LOCAL_THUMBNAIL_RATIO: 16 / 9, // 16:9
 
@@ -136,7 +138,7 @@ var interfaceConfig = {
      */
     MOBILE_APP_PROMO: true,
 
-    NATIVE_APP_NAME: 'Jitsi Meet',
+    NATIVE_APP_NAME: 'Tango Vision Meet',
 
     // Names of browsers which should show a warning stating the current browser
     // has a suboptimal experience. Browsers which are not listed as optimal or
@@ -145,7 +147,7 @@ var interfaceConfig = {
     OPTIMAL_BROWSERS: [ 'chrome', 'chromium', 'firefox', 'nwjs', 'electron', 'safari' ],
 
     POLICY_LOGO: null,
-    PROVIDER_NAME: 'Jitsi',
+    PROVIDER_NAME: 'Tango Vision',
 
     /**
      * If true, will display recent list
@@ -175,7 +177,7 @@ var interfaceConfig = {
      * If indicated some of the error dialogs may point to the support URL for
      * help.
      */
-    SUPPORT_URL: 'https://community.jitsi.org/',
+    SUPPORT_URL: 'https://support.tango.vision/',
 
     TOOLBAR_ALWAYS_VISIBLE: false,
 
